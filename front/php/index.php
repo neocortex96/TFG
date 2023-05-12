95bce2a80b18fe28e51b7b38c5b9bcc7fdeadf17
Mesut <mesut.shenturk@gmail.com>
2023-05-12 14:43:37 +0200

    prueba carpetas y fix de autocompletar

---
+++ front/php/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="10">
+    <title>Document</title>
+
+    <!-- JS LINK -->
+    <script src="../js/script.js"></script>
+    <!-- BOOTSTRAP LINK -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+        crossorigin="anonymous"></script>
+      
+
+    <head>
+      <!-- LINK CSS -->
+      <link rel="stylesheet" href="../css/index.css">
+    </head>
+
+<body>
+
+
+    <nav class="barra-navegacion navbar navbar-expand-lg bg-success">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">LOGO</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse " id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link" href="#">Iniciar Sesión</a>
+                    <a class="nav-link" href="#">Registrarse</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+
+    <!-- SIDEBAR -->
+
+    aklsdlsa
+
+
+    <?php
+    ?>
+</body>
+
+</html>
