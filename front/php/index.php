1b3a6a640dd570ac35e45f57bb809938b2378eb4
Mesut Shentyurk <mshentyurk@minsait.com>
2023-05-31 17:43:22 +0200

    pagina 'profile' creada

--- front/php/index.php
+++ front/php/index.php
@@ -8,7 +8,6 @@
     <meta http-equiv="refresh" content="10">
     <title>Document</title>
 
-    <head>
         <!-- BOOTSTRAP LINK -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
         <!-- BOOTSTRAP ICONS -->
@@ -23,7 +22,7 @@
 
 
     <!-- NAVBAR -->
-    <nav class="barra-navegacion navbar navbar-expand-lg p-3">
+    <nav class="barra-navegacion navbar navbar-expand-lg p-3 bg-dark">
         <div class="container-fluid">
 
             <a class="navbar-brand text-light" href="#">LOGO</a>
