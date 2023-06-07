3b1428ed170830d737523b65bd3fb701f4f04ce1
Mesut Shentyurk <mshentyurk@minsait.com>
2023-06-07 17:07:25 +0200

    un poco de avance

---
+++ front/js/profile.js
@@ -0,0 +1,7 @@
+// document.getElementById("subirPost").addEventListener("click", miFuncion);
+// const miFormulario = document.getElementById("miFormulario");
+
+// function miFuncion(){
+//     miFormulario.style.display="block";
+
+// }
